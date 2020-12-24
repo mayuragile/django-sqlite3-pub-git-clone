954e8efbc8e22dd2edf8eca5e9dd244899d7cf88
mayuragile <mayur.barbhaya@agileinfoways.com>
2020-12-24 23:33:41 +0530

    'added other host'

--- pubprj/settings.py
+++ pubprj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'abl!k^*gf*opq1#)vkeb$&0-aq9^959&9b3l-qlyfypa&@q1ub'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0','*']
 
 
 # Application definition
